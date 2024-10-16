640bd35fd2c3de7290156b3d2ea167c1a4de0f80
kotsxd19 <20240438@ricaldone.edu.sv>
2024-10-15 21:24:56 -0600

    a
    a

--- Vista/Login/frmRecuperacioneDeContra.Designer.cs
+++ Vista/Login/frmRecuperacioneDeContra.Designer.cs
@@ -30,14 +30,14 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnRegresar = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
             this.btnPreguntasSeguridad = new System.Windows.Forms.Button();
             this.btnCorreoElectronico = new System.Windows.Forms.Button();
-            this.btnRegresar = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -64,6 +64,24 @@
             this.panel1.Size = new System.Drawing.Size(482, 59);
             this.panel1.TabIndex = 1;
             // 
+            // btnRegresar
+            // 
+            this.btnRegresar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRegresar.FlatAppearance.BorderSize = 0;
+            this.btnRegresar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(112)))), ((int)(((byte)(119)))));
+            this.btnRegresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegresar.ForeColor = System.Drawing.Color.Black;
+            this.btnRegresar.Image = global::Proyecto.Properties.Resources.icons8_flecha_izquierda_25;
+            this.btnRegresar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRegresar.Location = new System.Drawing.Point(5, 3);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(39, 35);
+            this.btnRegresar.TabIndex = 9;
+            this.btnRegresar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnRegresar.UseVisualStyleBackColor = true;
+            this.btnRegresar.Click += new System.EventHandler(this.btnNuevoEmpleado_Click);
+            // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(251)))), ((int)(((byte)(225)))));
@@ -74,12 +92,22 @@
             this.panel2.Size = new System.Drawing.Size(482, 61);
             this.panel2.TabIndex = 2;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(69, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(342, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Elije un metodo de recuperacion de contraseña";
+            // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(37, 61);
+            this.label5.Location = new System.Drawing.Point(37, 31);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(178, 20);
             this.label5.TabIndex = 2;
@@ -90,21 +118,11 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(303, 61);
+            this.label4.Location = new System.Drawing.Point(303, 31);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(138, 20);
             this.label4.TabIndex = 1;
             this.label4.Text = "Correo electronico";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(69, 3);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(342, 20);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Elije un metodo de recuperacion de contraseña";
             // 
             // panel3
             // 
@@ -129,7 +147,7 @@
             this.btnPreguntasSeguridad.ForeColor = System.Drawing.Color.Black;
             this.btnPreguntasSeguridad.Image = global::Proyecto.Properties.Resources.icons8_preguntas_65;
             this.btnPreguntasSeguridad.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnPreguntasSeguridad.Location = new System.Drawing.Point(73, 84);
+            this.btnPreguntasSeguridad.Location = new System.Drawing.Point(73, 54);
             this.btnPreguntasSeguridad.Name = "btnPreguntasSeguridad";
             this.btnPreguntasSeguridad.Size = new System.Drawing.Size(73, 39);
             this.btnPreguntasSeguridad.TabIndex = 9;
@@ -146,31 +164,13 @@
             this.btnCorreoElectronico.ForeColor = System.Drawing.Color.Black;
             this.btnCorreoElectronico.Image = global::Proyecto.Properties.Resources.icons8_correo_electrónico_65;
             this.btnCorreoElectronico.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnCorreoElectronico.Location = new System.Drawing.Point(337, 84);
+            this.btnCorreoElectronico.Location = new System.Drawing.Point(337, 54);
             this.btnCorreoElectronico.Name = "btnCorreoElectronico";
             this.btnCorreoElectronico.Size = new System.Drawing.Size(74, 39);
             this.btnCorreoElectronico.TabIndex = 9;
             this.btnCorreoElectronico.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnCorreoElectronico.UseVisualStyleBackColor = true;
             this.btnCorreoElectronico.Click += new System.EventHandler(this.btnNuevoEmpleado_Click);
-            // 
-            // btnRegresar
-            // 
-            this.btnRegresar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnRegresar.FlatAppearance.BorderSize = 0;
-            this.btnRegresar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(112)))), ((int)(((byte)(119)))));
-            this.btnRegresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnRegresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRegresar.ForeColor = System.Drawing.Color.Black;
-            this.btnRegresar.Image = global::Proyecto.Properties.Resources.icons8_flecha_izquierda_25;
-            this.btnRegresar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnRegresar.Location = new System.Drawing.Point(5, 3);
-            this.btnRegresar.Name = "btnRegresar";
-            this.btnRegresar.Size = new System.Drawing.Size(39, 35);
-            this.btnRegresar.TabIndex = 9;
-            this.btnRegresar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnRegresar.UseVisualStyleBackColor = true;
-            this.btnRegresar.Click += new System.EventHandler(this.btnNuevoEmpleado_Click);
             // 
             // frmRecuperacioneDeContra
             // 
